3a56af10921c801140e21a50eed8131eeeb572d9
ElyriaG <ElyriaG@users.noreply.github.com>
2019-04-20 23:52:00 -0500

    Got value from button

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -12,33 +12,26 @@
 
 function renderButtons() {
     var buttons = ""
-    // $("#buttons-view").empty();
+    var movie = ""
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=FHh9mWC90FyTtVYHXSy5uFhHubUvyLWb&limit=1";
+
+    $("#searchForm").empty();
     var movies = ["Interstellar", "The Dark Knight", "Inception", "Pirates of the Carribean", "Anchorman", "Kill Bill", "Star Wars", "Lord of the Rings", "Harry Potter", "The Blues Brothers", "The Matrix", "Ace Ventura", "Dirty Dancing", "Spongebob the Movie", "Mad Max", "Venom", "The Avengers"];
     for (var i = 0; i < movies.length; i++) {
-       var buttons = $("<button>").attr("id", "moviesButton").text(movies[i])
+        var buttons = $("<button>").attr("id", "moviesButton").text(movies[i])
         buttons.appendTo("#buttonsDiv");
-        // console.log(buttons)
         $(buttons).on("click", function (event) {
-            // event.preventdefault();
-            var movie = $(this).val().trim();
-            console.log($(movie).val());
+            var movie = $(this).text();
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                $("#gifsDiv").append('<img class="gif" src="' + response.data[0].embed_url + '">');
+            });
         });
 
-        var movie = ""
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=FHh9mWC90FyTtVYHXSy5uFhHubUvyLWb&limit=1";
-        // $.ajax({
-        //     url: queryURL,
-        //     method: "GET"
-        // }).then(function (response) {
-        //     $("#gifsDiv").append('<img class="gif" src="' + response.data[0].embed_url +'">');
-        // });
-        // console.log("hello");
-        // $("#moviesButton").on("click", function (event) {
-        //     // event.preventdefault();
-        //     var movie = $(this).val();
-        //     console.log("moviesButton");
-        // });
-    }
+
+    };
 };
 
 renderButtons();
